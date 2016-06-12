daed30e3ad1d85096a2c2c714925123a90c40cf4
Priyam <patelpriyam10@yahoo.com>
2016-06-12 14:20:40 -0400

    6.12 changes

--- TrickBot.js
+++ TrickBot.js
@@ -8,63 +8,63 @@ var Discord = require("discord.js");
 	var input = message.content.toLowerCase();
 	if(input === "trkappaline")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkAppaline.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkAppaline.png");
 	}
 	if(input === "trkbronze")
 	{
-		//audio.src = "Projects//DiscordBot//audio//snoopdoge.mp3";
+		//audio.src = "audio//snoopdoge.mp3";
 		//audio.loop = false;
 		//audio.play();
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkBronze.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkBronze.png");
 	}
 	if(input === "trkdoge")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkDoge.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkDoge.png");
 	}if(input === "trkdorito")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkDorito.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkDorito.png");
 	}if(input === "trkfail")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkFail.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkFail.png");
 	}if(input === "trkgood")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkGood.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkGood.png");
 	}
 	if(input === "trkhorsey")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkHorsey.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkHorsey.png");
 	}
 	if(input === "trklata")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkLata.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkLata.png");
 	}
 	if(input === "trklegend")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkLegend.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkLegend.png");
 	}
 	if(input === "trklyfe")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkLyfe.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkLyfe.png");
 	}
 	if(input === "trkpcane")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkPcane.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkPcane.png");
 	}
 	if(input === "trkpleb")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkPleb.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkPleb.png");
 	}
 	if(input === "trkrip")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkRip.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkRip.png");
 	}
 	if(input === "trksalt")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkSalt.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkSalt.png");
 	}
 	if(input === "trkwave")
 	{
-		bot.sendFile(message, "Projects//DiscordBot//emotes//trkWave.png");
+		bot.sendFile(message, "C://Users//Priyam//Desktop//Dropbox//trkWave.png");
 	}
 
  });
